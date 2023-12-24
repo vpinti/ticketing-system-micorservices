396d249532fd608f69846cba7ce4ab7d6eea282c
vpinti <vittorio.pinti@gmail.com>
2023-12-24 14:27:14 +0100

    signout unit test

---
+++ auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,21 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("clears the cookie after signing out", async () => {
+    await request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "test@test.it",
+            password: "password"
+        })
+        .expect(201);
+
+    const response = await request(app)
+        .post("/api/users/signout")
+        .send({})
+        .expect(200);
+
+    expect(response.get("Set-Cookie")[0]).toEqual(  // [0] because it's an array
+        "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
+    );
+});
