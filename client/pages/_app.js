13aedffee4095c80f34eac1808f4d372f90f3fc2
vpinti <vittorio.pinti@gmail.com>
2023-12-26 19:42:17 +0100

    moved getInitialProps to App Component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 }
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return { pageProps, ...data };
+}
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,22 +1,9 @@
-import buildClient from "../api/build-client";
-import axios from "axios";
-
 const LandingPage = ({ currentUser }) => {
     return currentUser ? (
         <h1>You are signed in</h1>
     ) : (
         <h1>You are NOT signed in</h1>
     );
-};
-
-LandingPage.getInitialProps = async context => {
-    const client = buildClient(context);
-
-    const { data } = await client.get("/api/users/currentuser").catch(err => {
-        console.log(err.message);
-    });
-
-    return data;
 };
 
 export default LandingPage;
