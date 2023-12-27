f70f786dd1f7922b827150b267e7dce2e03f386c
vpinti <vittorio.pinti@gmail.com>
2023-12-27 19:45:38 +0100

    added header and signout component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,20 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import buildClient from "../api/build-client";
+import Header from "../components/header";
 
 const AppComponent = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-}
+    return (
+        <div>
+            <Header currentUser={pageProps.currentUser} />
+            <Component {...pageProps} />
+        </div>
+    );
+};
 
-AppComponent.getInitialProps = async (appContext) => {
-  const client = buildClient(appContext.ctx);
-  const { data } = await client.get('/api/users/currentuser');
+AppComponent.getInitialProps = async appContext => {
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get("/api/users/currentuser");
 
-  let pageProps = {};
-  if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
-  }
+    let pageProps = {};
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
 
-  return { pageProps, ...data };
-}
+    return { pageProps, ...data };
+};
 
 export default AppComponent;
