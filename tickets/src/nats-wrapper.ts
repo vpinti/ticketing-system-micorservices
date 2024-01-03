abf68c16c4a18c7cdd680bc774def814197a8ecf
vpinti <vittorio.pinti@gmail.com>
2024-01-03 15:18:28 +0100

    nats graceful shutdown

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -1,33 +1,26 @@
-import nats, { Stan } from 'node-nats-streaming';
+import nats, { Stan } from "node-nats-streaming";
 
-class NatsWrapper
-{
+class NatsWrapper {
     private _client?: Stan;
 
-    get client()
-    {
-        if (!this._client)
-        {
-            throw new Error('Cannot access NATS client before connecting');
+    get client() {
+        if (!this._client) {
+            throw new Error("Cannot access NATS client before connecting");
         }
 
         return this._client;
     }
 
-    connect(clusterId: string, clientId: string, url: string)
-    {
+    connect(clusterId: string, clientId: string, url: string) {
         this._client = nats.connect(clusterId, clientId, { url });
 
-        return new Promise<void>((resolve, reject) =>
-        {
-            this.client.on('connect', () =>
-            {
-                console.log('Connected to NATS');
+        return new Promise<void>((resolve, reject) => {
+            this.client.on("connect", () => {
+                console.log("Connected to NATS");
                 resolve();
             });
 
-            this.client.on('error', (err) =>
-            {
+            this.client.on("error", err => {
                 reject(err);
             });
         });
@@ -35,11 +28,3 @@ class NatsWrapper
 }
 
 export const natsWrapper = new NatsWrapper();
-
-// return new Promise<void>((resolve, reject) => {
-//     this._client!.on('connect', () => {
-//       console.log('Connected to NATS');
-//       resolve();
-//     });
-
-
