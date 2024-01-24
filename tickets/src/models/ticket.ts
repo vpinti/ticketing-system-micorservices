80eabe48f03a99d299d75fc2fd6aa6b061b7d7ea
vpinti <vittorio.pinti@gmail.com>
2024-01-24 21:26:24 +0100

    ticket events plus version

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 // An interface that describes the properties
 // that are required to create a new Ticket
@@ -20,6 +21,7 @@ interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
     userId: string;
+    version: number;
 }
 
 const ticketSchema = new mongoose.Schema({
@@ -40,10 +42,12 @@ const ticketSchema = new mongoose.Schema({
         transform(doc, ret) {
             ret.id = ret._id;
             delete ret._id;
-            delete ret.__v;
         }
     }
 });
+
+ticketSchema.set("versionKey", "version");
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs);
