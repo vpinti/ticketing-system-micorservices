1e2bb76ec4d2f9bd3f3bc85aa2f7ca502b5d3a6f
vpinti <vittorio.pinti@gmail.com>
2024-02-05 18:07:27 +0100

    enable listener to ticket service

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import { natsWrapper } from "../../nats-wrapper";
+import { Ticket } from "../../models/ticket";
 
 it("returns a 404 if the provided id does not exist", async () => {
     const id = global.createMongoId();
@@ -125,4 +126,29 @@ it("publishes an event", async () => {
         .expect(200);
 
     expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it("rejects updates if the ticket is reserved", async () => {
+    const cookie = global.signin();
+
+    const response = await request(app)
+        .post("/api/tickets")
+        .set("Cookie", cookie)
+        .send({
+            title: "asdfasdf",
+            price: 20,
+        });
+
+    const ticket = await Ticket.findById(response.body.id);
+    ticket!.set({ orderId: global.createMongoId() });
+    await ticket!.save();
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set("Cookie", cookie)
+        .send({
+            title: "new title",
+            price: 100,
+        })
+        .expect(400);
 });
